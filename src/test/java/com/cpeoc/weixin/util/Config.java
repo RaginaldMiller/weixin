25bda6dff63bad6c3a9471383c4e96ef51952d40
kk <kk@k>
2018-04-10 11:25:21 +0800

    修改测试手机配置

--- src/test/java/com/cpeoc/weixin/util/Config.java
+++ src/test/java/com/cpeoc/weixin/util/Config.java
@@ -1,7 +1,9 @@
 package com.cpeoc.weixin.util;
 
 public interface Config {
-	String serverHost = "192.168.31.119";
-	String deviceName = "28a38e6f";
+	//192.183.3.11  192.168.31.119
+	String serverHost = "192.183.3.11";
+	//192.183.252.128:5555  28a38e6f  127.0.0.1:62001
+	String deviceName = "192.183.252.128:5555";
 	String deviceSysVersion = "5.1";
 }
